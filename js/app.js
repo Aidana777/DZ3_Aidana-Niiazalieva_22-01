fda78a0cfa0bb015812e3fc8487c3c6d30e8d2a9
Aidana777 <aidananiazalieva27@gmail.com>
2023-02-20 17:32:54 +0600

    setInterval

--- js/app.js
+++ js/app.js
@@ -1,70 +1,96 @@
-const tabContent = document.querySelectorAll('.tabcontent');
-const tabs = document.querySelectorAll('.tabheader__item');
-const tabsParent = document.querySelector('.tabheader__items');
 
-const hidetabContent = () => {
-    tabContent.forEach((item) => {
-        item.style.display = 'none'
-    })
-    tabs.forEach((item) => {
-        item.classList.remove('tabheader__item_active')
-    })
+//* SLIDER
 
+let slideIndex = 0;
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-const showTabContent = (i = 0) => {
-    tabContent[i].style.display = 'block';
-    tabs[i].classList.add('tabheader__item_active')
-
-}
-
-hidetabContent();
-showTabContent()
-
-tabsParent.addEventListener('click', (event) => {
-    const target = event.target
-    if (target.classList.contains('tabheader__item')) {
-        tabs.forEach((item, index) => {
-            if (target === item) {
-                hidetabContent();
-                showTabContent(index)
-            }
-        })
+function showSlides(n) {
+    let slides = document.querySelectorAll(".tabheader__item");
+    let sliderSlide = document.querySelectorAll(".tabcontent");
+    if (n > slides.length && n > sliderSlide.length) {
+        slideIndex += 1;
     }
+    if (n < 0) {
+        slideIndex = slides.length && slideIndex === sliderSlide.length;
+    }
+    for (let slide of slides) {
+        slide.classList.remove("tabheader__item_active");
+    }
+    slides[n].classList.add("tabheader__item_active");
+
+    for (let slide of sliderSlide) {
+        slide.style.display = "none";
+    }
+    sliderSlide[slideIndex].style.display = "block";
+}
+
+let timer = setInterval(function () {
+    {
+        slideIndex++;
+        if (slideIndex > 3) {
+            slideIndex = 0
+        }
+    }
+    showSlides(slideIndex);
+}, 3000);
+
+//* MODAL
+
+const modal = document.querySelector(".modal");
+const modalTrigger = document.querySelectorAll("[data-modal]");
+
+modalTrigger.forEach((item) => {
+    item.addEventListener("click", openModal);
+});
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+
+    clearInterval(modalTimeout);
+}
+
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+}
+
+modal.addEventListener("click", (event) => {
+    if (
+        event.target === modal ||
+        event.target.classList.contains("modal__close")
+    ) {
+        closeModal();
+    }
+});
+
+function openModalScroll() {
+    const page = document.documentElement;
+
+    if (page.scrollTop + page.clientHeight >= page.scrollHeight) {
+        openModal();
+
+        window.removeEventListener("scroll", openModalScroll);
+    }
+}
+
+window.addEventListener("scroll", openModalScroll);
+const modalTimeout = setTimeout(openModal, 50000);
+
+const open = document.querySelector("#open_modal");
+const close = document.querySelector('#close_modal');
 
 
+open.addEventListener('click', () => {
+    modal.classList.remove('close_modal')
+    modal.classList.add('open_modal')
 })
 
-
-
-const modal = document.querySelector('.modal')
-const openModalBtn = document.querySelector('.btn_white');
-const closeModalBtn = document.querySelector('.modal__close')
-const openModal = () => {
-    modal.classList.add('show');
-    modal.classList.remove('hide')
-    document.body.style.overflow = 'hidden'
-};
-openModalBtn.addEventListener('click', openModal)
-
-
-const closeModal = () => {
-
-    modal.classList.remove('show');
-    modal.classList.add('hide')
-};
-closeModalBtn.addEventListener('click', closeModal)
-
-window.addEventListener('scroll', () => {
-    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = window.scrollY;
-    if (Math.ceil(scrolled) === scrollable) {
-        openModal(scrolled);
-    }
+close.addEventListener('click', () => {
+    modal.classList.remove('open_modal')
+    modal.classList.add('close_modal')
 })
-
-window.onclick=function(e){
-    if (e.target==modal) {
-        modal.style.display='none'
-    }
-}
