0cc6102be9116f02b9d478722b8f3b7a90c4db72
Aidana777 <aidananiazalieva27@gmail.com>
2023-02-21 12:17:16 +0600

    elem

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ const tabsParent = document.querySelector(".tabheader__items");
 const tabContent = document.querySelectorAll(".tabcontent");
 
 const hideTabContent = () => {
-    tabContent.forEach((item) => {
-        item.style.display = "none";
-    });
-    tabs.forEach((item) => {
-        item.classList.remove("tabheader__item_active");
-    });
+  tabContent.forEach((item) => {
+    item.style.display = "none";
+  });
+  tabs.forEach((item) => {
+    item.classList.remove("tabheader__item_active");
+  });
 };
 
 const showTabContent = (i = 0) => {
-    tabContent[i].style.display = "block";
-    tabs[i].classList.add("tabheader__item_active");
+  tabContent[i].style.display = "block";
+  tabs[i].classList.add("tabheader__item_active");
 };
 hideTabContent();
 showTabContent();
@@ -21,52 +21,52 @@ showTabContent();
 let currentSlideIndex = 0;
 
 const startSlider = () => {
-    const sliderInterval = setInterval(() => {
-        currentSlideIndex = (currentSlideIndex + 1) % tabs.length;
-        hideTabContent();
-        showTabContent(currentSlideIndex);
-    }, 1000);
+  const sliderInterval = setInterval(() => {
+    currentSlideIndex = (currentSlideIndex + 1) % tabs.length;
+    hideTabContent();
+    showTabContent(currentSlideIndex);
+  }, 1000);
 
-    return sliderInterval;
+  return sliderInterval;
 };
 
 let sliderInterval = startSlider();
 
 tabsParent.addEventListener("click", (event) => {
-    const target = event.target;
+  const target = event.target;
 
-    if (target.classList.contains("tabheader__item")) {
-        clearInterval(sliderInterval);
+  if (target.classList.contains("tabheader__item")) {
+    clearInterval(sliderInterval);
 
-        tabs.forEach((item, i) => {
-            if (target === item) {
-                currentSlideIndex = i;
-                hideTabContent();
-                showTabContent(i);
-            }
-        });
+    tabs.forEach((item, i) => {
+      if (target === item) {
+        currentSlideIndex = i;
+        hideTabContent();
+        showTabContent(i);
+      }
+    });
 
-        setTimeout(() => {
-            sliderInterval = startSlider();
-        }, 3000);
-    }
+    setTimeout(() => {
+      sliderInterval = startSlider();
+    }, 3000);
+  }
 });
 
 const animateTabContent = (elem) => {
-    elem.classList.add("fade");
-    setTimeout(() => {
-        elem.classList.remove("fade");
-    }, 500);
+  elem.classList.add("fade");
+  setTimeout(() => {
+    elem.classList.remove("fade");
+  }, 500);
 };
 
 const animateTabs = () => {
-    tabs.forEach((tab) => {
-        animateTabContent(tab);
-    });
+  tabs.forEach((tab) => {
+    animateTabContent(tab);
+  });
 };
 
 setInterval(() => {
-    animateTabs();
+  animateTabs();
 }, 1000);
 
 const modal = document.querySelector(".modal");
